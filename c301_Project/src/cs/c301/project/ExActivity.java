4e6294f66a17f738a4755a87a80f4a9021ca7343
wjtran <wjtran@ualberta.ca>
2012-03-07 17:59:31 -0700

    .

--- c301_Project/src/cs/c301/project/ExActivity.java
+++ c301_Project/src/cs/c301/project/ExActivity.java
@@ -2,12 +2,23 @@ package cs.c301.project;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class ExActivity extends Activity {
+public class ExActivity extends Activity implements OnClickListener {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.group_list);
+        
+        Button button = (Button) findViewById(R.id.group_back);
+        button.setOnClickListener(this);
     }
+
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 }
