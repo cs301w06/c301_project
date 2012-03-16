59cc88555945fc72d768edf9ad0c4276150ae221
Yang H <yhu3@ualberta.ca>
2012-03-15 23:47:38 -0600

    minor change

--- src/cs/c301/project/GroupList.java
+++ src/cs/c301/project/GroupList.java
@@ -1,11 +1,12 @@
 package cs.c301.project;
 
 import java.io.File;
-import java.util.Date;
 import java.util.Vector;
+import java.util.WeakHashMap;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -26,10 +27,12 @@ public class GroupList extends Activity implements PhotoModelListener {
 	private File[] items;
 	private boolean isUnderReview;
 	private ListView lv;
-	
+	public WeakHashMap<Integer, AlertDialog.Builder> dialogs;
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.grouplist);
+		
+		dialogs = new WeakHashMap<Integer, AlertDialog.Builder>();
 		
 		//the filepath for the storage path is stored in this intent
 		Bundle extra = getIntent().getExtras();
@@ -126,7 +129,9 @@ public class GroupList extends Activity implements PhotoModelListener {
     			//do nothing because user cancelled
     		}
     	});
-	
+    	
+    	dialogs.put(0, newGroupDialog);
+
 		newGroupDialog.show();
 	}
     
