23dfde016b3857e98b4f43179c80c2a9c1fa8f62
wjtran <wjtran@ualberta.ca>
2012-03-15 21:37:20 -0600

    Javadoc for photo review

--- src/cs/c301/project/PhotoReview.java
+++ src/cs/c301/project/PhotoReview.java
@@ -14,40 +14,60 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.Toast;
 import cs.c301.project.Data.PhotoEntry;
 import cs.c301.project.Listeners.PhotoModelListener;
 
+/**
+ * Take recent taken photo from the sd card and draws the photo up on screen
+ * for the user to review and decide whether to put into group and then keep 
+ * the photo or user can discard the photo if it is unwanted
+ *
+ */
 public class PhotoReview extends Activity implements PhotoModelListener {
+
 	ListView partlist;
 	Button keepButton;
 	ProgressDialog p;
 	private String groupName;
 	private PhotoEntry photoEntry;
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.review);
-	
-		/* Review of photo */
-		ImageView reviewPhoto = (ImageView) findViewById(R.id.review_photo);
-//		reviewPhoto.setImageDrawable(Drawable.createFromPath(photoEntry.getFilePath()));
-//		
-		/* Comparasion photo */
-		ImageView comparePhoto = (ImageView) findViewById(R.id.review_photoCompare);
 
-		/* Discard button */
+		/** 
+		 * Image view of the most recent photo taken
+		 */
+		ImageView reviewPhoto = (ImageView) findViewById(R.id.review_photo);
+		reviewPhoto.setImageDrawable(Drawable.createFromPath(photoEntry.getFilePath()));
+
+		/**
+		 * Comparision image view of recent photo taken 
+		 */
+		//ImageView comparePhoto = (ImageView) findViewById(R.id.review_photoCompare);
+
+		/** 
+		 * Discard button to allow the user to discard unwanted photos
+		 */
 		Button discardButton = (Button) findViewById(R.id.review_disc);
+
+		/**
+		 * Allow user to discard the photo currently on screen
+		 */
 		discardButton.setOnClickListener(new OnClickListener() {
 
 			public void onClick(View arg0) {
 
+				PhotoApplication.removePhoto(photoEntry.getID());
 			}
 
 		});
 
-		/* Select group and keep button */
+		/**
+		 *  Allow user to choose group for the current on screen photo
+		 *  and will save into chosen group
+		 */
 		keepButton = (Button) findViewById(R.id.review_keep);
 		keepButton.setText("Select Group");
 		keepButton.setOnClickListener(new Button.OnClickListener(){
@@ -58,57 +78,80 @@ public class PhotoReview extends Activity implements PhotoModelListener {
 				startActivityForResult(intent, 0);
 			}			
 		});
-		
+
 		PhotoApplication.addPhotoModelListener(this);
 	}
-	
-    @Override
-    protected void onActivityResult(int a, int b, Intent intent) {
-    	Bundle extra = intent.getExtras();
-    	
-    	groupName = extra.getString("groupname");
-    	photoEntry.setGroup(groupName);
-    }
-    
+
+	/**
+	 * Grab the group name of the photo from the group list intent
+	 * @param a	
+	 * @param b 		
+	 * @param intent	extra data from the intent
+	 */
+	@Override
+	protected void onActivityResult(int a, int b, Intent intent) {
+		Bundle extra = intent.getExtras();
+
+		groupName = extra.getString("groupname");
+		photoEntry.setGroup(groupName);
+	}
+
+	/**
+	 * Grab the latest photo from the sd card and give the global 
+	 * variable the string path
+	 * 
+	 * @param photos	vector list of photos in the sd card
+	 */
 	public void photosChanged(Vector<PhotoEntry> photos) {
 		int id = -1;
 		Date latestDate = null;
-		
+
 		for (int i = 0; i < photos.size(); i++) {
 			if (latestDate == null) {
 				photos.elementAt(i).getDate();
 				id = i;
 			} else {
 				Date tempDate = photos.elementAt(i).getDate();
-				
+
 				if (tempDate.compareTo(latestDate) > 0) {
 					id = i;
 					latestDate = tempDate;
 				}
 			}
 		}
-		
+
 		if (id != -1) {
 			photoEntry = photos.elementAt(id);
-			
+
 			onStart();
 		}
 	}
 
+	/**
+	 * Show the review photo page and draw the photo on screen
+	 */
 	protected void onStart() {
 		super.onStart();
 		ImageView reviewPhoto = (ImageView) findViewById(R.id.review_photo);
 		reviewPhoto.setImageDrawable(Drawable.createFromPath(photoEntry.getFilePath()));
 	}
-	
+
+	/**
+	 * (non-Javadoc)
+	 * @see cs.c301.project.Listeners.PhotoModelListener#tagsChanged(java.util.Vector)
+	 */
 	public void tagsChanged(Vector<String> tags) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	/** 
+	 * (non-Javadoc)
+	 * @see cs.c301.project.Listeners.PhotoModelListener#groupsChanged(Vector)
+	 */
 	public void groupsChanged(Vector<String> groups) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
