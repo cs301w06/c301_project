f5685526e9e537d804ebcdcb711aa9ca5061587a
wjtran <wjtran@ualberta.ca>
2012-03-15 21:45:41 -0600

    got rid of warnings

--- src/cs/c301/project/SearchPhotoView.java
+++ src/cs/c301/project/SearchPhotoView.java
@@ -1,18 +1,10 @@
 package cs.c301.project;
 
-import java.util.Collection;
 import java.util.Vector;
-
 import cs.c301.project.Data.PhotoEntry;
 import cs.c301.project.Listeners.PhotoModelListener;
-
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-
 
 public class SearchPhotoView extends Activity implements PhotoModelListener {
 	@Override
