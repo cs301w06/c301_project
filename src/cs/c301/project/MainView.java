78a52dd2e406149aeff0d7e681e6a90052a3aec6
Yang H <yhu3@ualberta.ca>
2012-03-15 20:13:31 -0600

    Changes of photos

--- src/cs/c301/project/MainView.java
+++ src/cs/c301/project/MainView.java
@@ -1,16 +1,22 @@
 package cs.c301.project;
 
+import java.io.File;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import cs.c301.project.Data.PhotoEntry;
 
 
 public class MainView extends Activity {
+	
+	private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -23,11 +29,13 @@ public class MainView extends Activity {
 
 				public void onClick(View arg0) {
 				
-				/* Save into temporary folder */
-				String folder = "tmp";
-				String file = "temp"; // change later
-				Photo.savePhoto(folder, file);
-				getPhoto();
+//				/* Save into temporary folder */
+//				String folder = "tmp";
+//				String file = "temp"; // change later
+//				Photo.savePhoto(folder, file);
+//				getPhoto();
+					
+				requestPhotoCapture();
 			}
 			
 		});
@@ -54,24 +62,18 @@ public class MainView extends Activity {
 
 		});
 	}
-	private Photo photo;
-
-	/**
-	 * Getter of the property <tt>photo</tt>
-	 * @return  Returns the photo.
-	 * @uml.property  name="photo"
-	 */
 	
-    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
 	
-    protected void getPhoto() {
-
+	public void requestPhotoCapture() {
 		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+		PhotoEntry entry = PhotoApplication.getTemporaryImage();
+		File imageFile = new File(entry.getFilePath());
+		Uri imageUri = Uri.fromFile(imageFile);
+		intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+		PhotoApplication.addPhoto(entry);
 		
-		Photo.savePhoto("tmp", "temp");
-		intent.putExtra(MediaStore.EXTRA_OUTPUT, Photo.getPhoto("tmp", "temp"));
 		startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);		
-		
 	}
 
 	/**
@@ -83,9 +85,8 @@ public class MainView extends Activity {
 	protected void onActivityResult(int requestCode, int resultCode, Intent intent){
 		
 		if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
-			
 			if (resultCode == RESULT_OK) {
-				Intent aIntent = new Intent(MainView.this, PhotoReview.class);
+				Intent aIntent = new Intent(getApplicationContext(), PhotoReview.class);
 				startActivity(aIntent);
 			}
 			
