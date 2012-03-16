527ffe9f0bc2b83f46bcced43f85489c6544b0fa
wjtran <wjtran@ualberta.ca>
2012-03-15 21:42:35 -0600

    Main view javadoc update

--- src/cs/c301/project/MainView.java
+++ src/cs/c301/project/MainView.java
@@ -1,19 +1,21 @@
 package cs.c301.project;
 
 import java.io.File;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import cs.c301.project.Data.PhotoEntry;
 
-
+/**
+ * The main page of they project, gives choices for user to chose from.  
+ * User can choose to take a photo, list the photos already taken, and search
+ * the environment (saved data) for saved photos
+ */
 public class MainView extends Activity {
 	
 	private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
@@ -24,31 +26,16 @@ public class MainView extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		
-		/* Camera button to direct to CameraView page */
+		/**
+		 *  Camera button to direct to CameraView page 
+		 */
 		Button cameraButton = (Button) findViewById(R.id.main_camera);
 		cameraButton.setOnClickListener(new OnClickListener() {
 
 				public void onClick(View arg0) {
 				
-//				/* Save into temporary folder */
-//				String folder = "tmp";
-//				String file = "temp"; // change later
-//				Photo.savePhoto(folder, file);
-//				getPhoto();
-					
 					PhotoEntry entry = PhotoApplication.getTemporaryImage();
 					
-//					// Folder destination so that photos can be saved here
-//					String folder = PhotoApplication.getFilePath() + "/tmp";
-//					// Create a new file
-//					File folderF = new File(folder);
-//					// Create folder if does not exist
-//					if (!folderF.exists()) {
-//						folderF.mkdir();
-//					}
-//					// Save image in folder and current time as name
-//					String imageFilePath = folder + "/temp.jpg";
-					// Create Uri
 					File imageFile = new File(entry.getFilePath());
 					Uri imageUri = Uri.fromFile(imageFile);
 	
@@ -62,7 +49,7 @@ public class MainView extends Activity {
 			
 		});
 
-		/* List button to direct to PartsListView page */
+		/** List button to direct to PartsListView page */
 		Button listButton = (Button) findViewById(R.id.main_list);
 		listButton.setOnClickListener(new OnClickListener() {
 
@@ -72,7 +59,7 @@ public class MainView extends Activity {
 			}
 		});
 		
-		/* Search button to direct to SearchPhoto page */
+		/** Search button to direct to SearchPhoto page */
 		Button searchButton = (Button) findViewById(R.id.main_search);
 		searchButton.setOnClickListener(new OnClickListener() {
 
@@ -85,11 +72,6 @@ public class MainView extends Activity {
 	}
 	
 
-	/**
-	 * Setter of the property <tt>photo</tt>
-	 * @param photo  The photo to set.
-	 * @uml.property  name="photo"
-	 */
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent intent){
 		
