0686b962c6df92c8f9679d0606676a2568fae546
wjtran <wjtran@ualberta.ca>
2012-04-03 08:05:51 -0600

    Got bogo pic to work with sql, still problems with the keep button (not adding the photo)

--- src/cs/c301/project/PhotoReview.java
+++ src/cs/c301/project/PhotoReview.java
@@ -2,9 +2,7 @@ package cs.c301.project;
 
 import java.util.Date;
 import java.util.Vector;
-
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
@@ -12,51 +10,40 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.Toast;
 import cs.c301.project.Data.PhotoEntry;
 
 /**
  * Take recent taken photo from file and draws the photo up on screen
- * for the user to review and decide whether to put into group and then keep 
+ * for the user to review and decide whether to put into group and then keep
  * the photo or user can discard the photo if it is unwanted
  *
  */
 public class PhotoReview extends Activity {
 
-	ListView partlist;
-	Button groupButton, keepButton;
-	ProgressDialog p;
+	private Button groupButton, keepButton;
 	private String groupName;
 	private PhotoEntry photoEntry;
-	private boolean isGroupSelected;
-	
+	private Bitmap newBMP;
+
 	@Override
-	/**
-	 * Method called upon activity creation
-	 */
+	/** Method called upon activity creation */
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.review);
-		
-		isGroupSelected = false;
-		
-		Bundle extra = getIntent().getExtras();
-		
-		photoEntry = (PhotoEntry)extra.getSerializable("photo");
-		
-		ImageView reviewPhoto = (ImageView) findViewById(R.id.review_photo);
-		reviewPhoto.setImageBitmap(photoEntry.getBitmap());
-		
+
+		setBogoPic();
+
 		Button discardButton = (Button) findViewById(R.id.review_disc);
 		discardButton.setOnClickListener(new OnClickListener() {
 
-			public void onClick(View arg0) {
+			public void onClick(View v) {
+
+				//PhotoApplication.removePhoto(photoEntry.getID());
 				Toast.makeText(getApplicationContext(), "Photo Discarded", Toast.LENGTH_SHORT).show();
 				Intent intent = new Intent(getApplication(), MainView.class);
 				startActivity(intent);
 			}
-
 		});
 
 		groupButton = (Button) findViewById(R.id.review_group);
@@ -67,43 +54,96 @@ public class PhotoReview extends Activity {
 				Intent intent = new Intent(getApplication(), GroupList.class);
 				intent.putExtra("isUnderReview", true);
 				startActivityForResult(intent, 0);
-			}			
+			}
 		});
 
 		keepButton = (Button) findViewById(R.id.review_keep);
-		keepButton.setOnClickListener(new Button.OnClickListener() {
+		keepButton.setOnClickListener(new Button.OnClickListener(){
 
 			public void onClick(View v) {
-				if (isGroupSelected) {
-					Intent intent = new Intent(getApplication(), PhotoSubView.class);
-					PhotoApplication.addPhoto(photoEntry);
-					Toast.makeText(getApplicationContext(), "Photo Saved", Toast.LENGTH_SHORT).show();
-					startActivity(intent);
-				} else {
-					Toast.makeText(getApplicationContext(), "Please select a group first", Toast.LENGTH_SHORT).show();
-				}
+				Toast.makeText(getApplicationContext(), "Please select a group first", Toast.LENGTH_SHORT).show();
 			}
-
 		});
 	}
 
 	/**
 	 * Grab the group name of the photo from the group list intent
-	 * @param a	
-	 * @param b 		
-	 * @param intent	extra data from the intent
+	 * @param requestCode Integer request code originally supplied, allowing you to identify who
+	 * this result came from
+	 * @param resultCode Integer result code returned by child activity through its setResult()
+	 * @param intent Intent, which can return result data to caller
 	 */
 	@Override
-	protected void onActivityResult(int a, int b, Intent intent) {	
+	protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+
 		try {
 			Bundle extra = intent.getExtras();
-
-			groupName = extra.getString("group");
-			photoEntry.setGroup(groupName);
+			final PhotoEntry newPhoto = new PhotoEntry();
 			
-			isGroupSelected = true;
+			groupName = extra.getString("groupname");
+			newPhoto.setGroup(groupName);
+			newPhoto.setBitmap(newBMP);
+			
+			keepButton.setOnClickListener(new Button.OnClickListener() {
+
+				public void onClick(View v) {
+					Intent intent = new Intent(getApplication(), PhotoSubView.class);
+					PhotoApplication.addPhoto(newPhoto);
+					Toast.makeText(getApplicationContext(), "Photo Saved", Toast.LENGTH_SHORT).show();
+					startActivity(intent);
+				}
+			});
+		}
+		catch (Exception e) {}
+	}
+
+	/**
+	 * Grab the latest photo from file and give the global
+	 * variable the string path
+	 *
+	 * @param photos vector list of photos on file
+	 */
+	public void photosChanged(Vector<PhotoEntry> photos) {
+
+		int id = -1;
+		Date latestDate = null;
+
+		for (int i = 0; i < photos.size(); i++) {
+
+			if (latestDate == null) {
+
+				photos.elementAt(i).getDate();
+				id = i;
+
+			} else {
+
+				Date tempDate = photos.elementAt(i).getDate();
+
+				if (tempDate.compareTo(latestDate) > 0) {
+					id = i;
+					latestDate = tempDate;
+				}
+			}
 		}
 
-		catch (Exception e) {}
+		if (id != -1) {
+			photoEntry = photos.elementAt(id);
+			onStart();
+		}
+	}
+
+	/** Show the review photo page and draw the photo on screen */
+	protected void onStart() {
+		super.onStart();
+
+		ImageView reviewPhoto = (ImageView) findViewById(R.id.review_photo);
+		reviewPhoto.setImageBitmap(newBMP);
+
+		// ImageView comparePhoto = (ImageView) findViewById(R.id.review_photoCompare);
+	}
+
+	/** Generate new bmp */
+	private void setBogoPic() {
+		newBMP = BogoPicGen.generateBitmap(400, 400);
 	}
 }
